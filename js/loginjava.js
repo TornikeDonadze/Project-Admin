4dd92091b237ceba77e28c14f78ec12dbbfe513c
TornikeusDonadzeusThe3rd <progamerdato69@gmail.com>
2023-04-07 18:39:14 +0400

    gaming

---
+++ js/loginjava.js
@@ -0,0 +1,40 @@
+const forms = document.querySelector(".forms"),
+pwShowHide = document.querySelectorAll(".eye-icon"),
+links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+eyeIcon.addEventListener("click", () => {
+  let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+  
+  pwFields.forEach(password => {
+      if(password.type === "password"){
+          password.type = "text";
+          eyeIcon.classList.replace("bx-hide", "bx-show");
+          return;
+      }
+      password.type = "password";
+      eyeIcon.classList.replace("bx-show", "bx-hide");
+  })
+  
+})
+})      
+
+links.forEach(link => {
+link.addEventListener("click", e => {
+ e.preventDefault();
+ forms.classList.toggle("show-signup");
+})
+})
+
+function func(){
+    let email = document.getElementById('gmailo').value;
+    let pass = document.getElementById('passwordo').value;
+    if (email == 'Admin@gmail.com' && pass == 'Admin1'){
+        location.href='adminmain.html';
+        alert("login successfull!")
+        
+    }
+    else{
+        alert("Entry Invalid")
+    }
+    }
